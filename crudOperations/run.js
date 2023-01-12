ae000513f83bdc804a866ed30c9ebdd1520cc4a9
Cromwell Patrick Davey <cromwellpatrick.davey@gmail.com>
2023-01-13 00:12:53 +0800

    Finalizes run.js

--- crudOperations/run.js
+++ crudOperations/run.js
@@ -1,2 +1,16 @@
 const Student = require("../src/studentsCrud.js");
 
+//// Test running the application
+const run = async() => {
+    try
+    {
+        const newStudent = new Student();
+        newStudent.testRun();
+    }
+    catch(err)
+    {
+        console.error(err);
+    }
+}
+
+run();
